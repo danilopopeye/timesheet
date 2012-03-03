72bfdc3c5a855ccefba04cbcdbe8ad525bb8596b
Danilo Sousa <eu@danilosousa.net>
2012-03-03 03:03:02 -0300

    Model Options with better defaults;

--- public/javascripts/models/options.js
+++ public/javascripts/models/options.js
@@ -1,10 +1,13 @@
 define(['backbone', 'localstorage'], function(Backbone, Store){
   return Backbone.Model.extend({
     localStorage: new Store('options'), 
-    month: null,
-    year: null,
     text: function(){
       return [ this.get('year'), this.get('month') ].join('-');
+    },
+    defaults: {
+      month: null,
+      year: null,
+      id: 'timesheet-options' // localStorage need a id
     }
   });
 });
