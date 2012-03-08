a7fc1be8f86d6e62831b148ca458657ead85472d
Danilo Sousa <eu@danilosousa.net>
2012-03-08 16:06:03 -0300

    Added the validation for entrance and exit as valid hour;

--- public/javascripts/models/day.js
+++ public/javascripts/models/day.js
@@ -2,20 +2,24 @@ define([ 'backbone', 'utils' ], function(Backbone, Utils){
   return Backbone.Model.extend({
     defaults: {
       date: new Date,
-      exit_hour: '--',
-      exit_minute: '--',
+      entrance: '--:--',
       minimum: '--:--',
-      maximum: '--:--'
-    },
-
-    initialize: function(){
-      this.set({
-        date: new Date( this.get('date') )
-      });
+      maximum: '--:--',
+      exit: '--:--'
     },
 
     today: function(){
       return Utils.isSameDay( this.get('date'), new Date() );
+    },
+
+    validate: function(attr, errors){
+      if( ! Utils.isValidHour( attr.entrance ) ){
+        return "The entrance must be a valid hour ( hh:mm )";
+      }
+
+      if( ! Utils.isValidHour( attr.exit ) ){
+        return "The exit must be a valid hour ( hh:mm )";
+      }
     }
   });
 });
