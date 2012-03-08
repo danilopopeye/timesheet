e1bfed719f64ba12c9cfa7bd06e0e514cc645b50
Danilo Sousa <eu@danilosousa.net>
2012-03-08 19:02:53 -0300

    Added the calculator module from overtime;
    https://github.com/BrunoGrasselli/overtime

---
+++ public/javascripts/calculator.js
@@ -0,0 +1,34 @@
+define(function(){
+  var timeAsString = function(time) {
+    return twoDigits(time.getHours()) + ":" + twoDigits(time.getMinutes());
+  };
+
+  var twoDigits = function(string) {
+    var collection = ("00" + string).split("");
+    return collection[collection.length - 2] + collection[collection.length - 1];
+  }
+
+  var convertTime = function(arrivedAt, period) {
+    var collection = arrivedAt.split(/[^0-9]/);
+    var hours = collection[0];
+    var minutes = collection[1];
+
+    var dateTime = new Date(2011, 01, 01, hours, minutes);
+    var result = new Date(dateTime.getTime() + period);
+    return timeAsString(result);
+  };
+
+  return {
+    minTime: function(arrivedAt) {
+      return convertTime(arrivedAt, ((9 * 60 + 33) * 60 * 1000));
+    },
+
+    regularTime: function(arrivedAt) {
+      return convertTime(arrivedAt, ((9 * 60 + 48) * 60 * 1000));
+    },
+
+    maxTime: function(arrivedAt) {
+      return convertTime(arrivedAt, ((9 * 60 + 63) * 60 * 1000));
+    }
+  };
+});
